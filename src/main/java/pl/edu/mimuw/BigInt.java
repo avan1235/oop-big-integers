395425a2c25db2dbe45b5dc266fddd579fe5e076
etiaro <j.klimek2002@gmail.com>
2022-03-23 19:06:22 +0100

    parsing string, addition, toString

--- src/main/java/pl/edu/mimuw/BigInt.java
+++ src/main/java/pl/edu/mimuw/BigInt.java
@@ -9,7 +9,13 @@ public final class BigInt {
   private final boolean isPositive;
 
   public BigInt(String number) {
-    throw new IllegalStateException("TODO task 1: parse number from valid String");
+    isPositive = number.charAt(0) != '-';
+
+    digits = new int[number.length() - (isPositive ? 0 : 1)];
+
+    for (int i = isPositive ? 0 : 1; i < number.length(); i++) {
+      digits[number.length() - i - 1] = number.charAt(i) - '0';
+    }
   }
 
   public BigInt(int[] digits, boolean isPositive) {
@@ -27,7 +33,48 @@ public final class BigInt {
   }
 
   public BigInt add(BigInt other) {
-    throw new IllegalStateException("TODO task 2: implement addition");
+    int[] digits = new int[Math.max(this.digits.length, other.digits.length) + 1];
+    boolean isPositive = true;
+
+    for (int i = 0; i < digits.length - 1; i++){
+      if (i < this.digits.length)
+        if(this.isPositive)
+          digits[i] += this.digits[i];
+        else
+          digits[i] -= this.digits[i];
+
+      if (i < other.digits.length)
+        if(other.isPositive)
+          digits[i] += other.digits[i];
+        else
+          digits[i] -= other.digits[i];
+
+      if(digits[i] > 10) {
+        digits[i+1] += 1;
+        digits[i] %= 10;
+      } else if (digits[i] < 0) {
+        digits[i+1]--;
+        digits[i] += 10;
+      }
+    }
+
+    if (digits[digits.length - 1] < 0) {
+      isPositive = false;
+      for(int i = 0; i < digits.length - 1; i++) {
+        digits[i] *= -1;
+        if(digits[i] < 0){
+          digits[i] += 10;
+          digits[i+1] += 1; // it will be multiplied by -1, so +1 is like -1
+        }
+      }
+      digits[digits.length - 1] *= -1;
+    }
+
+    int zeros = 0;
+    while(zeros < digits.length - 1 && digits[digits.length - zeros - 1] == 0) zeros++;
+    int[] resDigits = Arrays.copyOf(digits, digits.length - zeros);
+
+    return new BigInt(resDigits, isPositive);
   }
 
   public BigInt times(BigInt other) {
@@ -36,7 +83,10 @@ public final class BigInt {
 
   @Override
   public String toString() {
-    throw new IllegalStateException("TODO task 4: implement creating representation");
+    String res = this.isPositive ? "" : "-";
+    for(int i = this.digits.length - 1; i >= 0; i--)
+      res += digits[i];
+    return res;
   }
 
   @Override
--- src/main/java/pl/edu/mimuw/Main.java
+++ src/main/java/pl/edu/mimuw/Main.java
@@ -3,7 +3,10 @@ package pl.edu.mimuw;
 public class Main {
 
   public static void main(String[] args) {
-    final var number = new BigInt("42");
+    final var number = new BigInt("-10");
+    final var number2 = new BigInt("2");
     System.out.println(number);
+    System.out.println(number2);
+    System.out.println(number.add(number2));
   }
 }
