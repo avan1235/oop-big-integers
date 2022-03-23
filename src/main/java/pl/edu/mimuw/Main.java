0368e500e9f9aad3d3d3d48bfa1b0900128e9cf1
jszot <j.szot@student.uw.edu.pl>
2022-03-23 15:13:16 +0100

    argument printing

--- src/main/java/pl/edu/mimuw/Main.java
+++ src/main/java/pl/edu/mimuw/Main.java
@@ -3,6 +3,9 @@ package pl.edu.mimuw;
 public class Main {
 
   public static void main(String[] args) {
+    for (String arg : args) {
+      System.out.println(arg + " ");
+    }
     final var number = new BigInt("42");
     System.out.println(number);
   }
