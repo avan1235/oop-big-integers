58ef46e8a553db103ea8900b640f35a0be6f6b0a
Artur Kamieniecki <kamart02@wp.pl>
2022-03-24 15:45:30 +0100

    Automated tests test

--- src/main/java/pl/edu/mimuw/Main.java
+++ src/main/java/pl/edu/mimuw/Main.java
@@ -3,7 +3,7 @@ package pl.edu.mimuw;
 public class Main {
 
   public static void main(String[] args) {
-    final var number = new BigInt("733");
+    final var number = new BigInt("111");
     System.out.println(number);
   }
 }
