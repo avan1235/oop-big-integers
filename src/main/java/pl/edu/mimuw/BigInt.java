956197be49098a7dcab3521cc8386cbbe63c2518
m1kush <m1kush@disroot.org>
2022-03-26 13:47:20 +0100

    .

--- src/main/java/pl/edu/mimuw/BigInt.java
+++ src/main/java/pl/edu/mimuw/BigInt.java
@@ -2,6 +2,10 @@ package pl.edu.mimuw;
 
 import java.util.Arrays;
 
+import static java.lang.Math.abs;
+import static java.lang.Math.min;
+import static java.util.Collections.swap;
+
 public final class BigInt {
 
   private final int[] digits;
@@ -9,7 +13,14 @@ public final class BigInt {
   private final boolean isPositive;
 
   public BigInt(String number) {
-    throw new IllegalStateException("TODO task 1: parse number from valid String");
+    if (number.charAt(0) == '-') {
+      this.isPositive = false;
+    }
+    else this.isPositive = true;
+    this.digits = new int[number.length() - (this.isPositive ? 0 : 1)];
+    for (int i = 0;i<this.digits.length;i++) {
+      this.digits[i] = number.charAt(i + (this.isPositive ? 0 : 1));
+    }
   }
 
   public BigInt(int[] digits, boolean isPositive) {
@@ -27,7 +38,32 @@ public final class BigInt {
   }
 
   public BigInt add(BigInt other) {
-    throw new IllegalStateException("TODO task 2: implement addition");
+    if (other.digits.length > this.digits.length)
+      return other.add(this);
+    if (!this.isPositive)
+      return this.neg().add(other.neg()).neg();
+    if (!other.isPositive)
+      for (int i = 0;i<other.digits.length;i++)
+        other.digits[i] = -other.digits[i];
+
+    for (int i = 0;i<other.digits.length-1;i++) {
+      this.digits[this.digits.length - i - 1] += other.digits[other.digits.length - i - 1];
+      if (this.digits[this.digits.length - i - 1] > 9)
+      {
+        this.digits[this.digits.length - i - 1] -= 10;
+        this.digits[this.digits.length - i - 2]++;
+      }
+    }
+    this.digits[0] += other.digits[0];
+    if (this.digits[0] > 9)
+    {
+      this.digits[0] -= 10;
+      int[] ret = new int[this.digits.length + 1];
+      ret[0] = 1;
+      System.arraycopy(this.digits, 0, ret, 1, this.digits.length);
+      return new BigInt(ret, true);
+    }
+
   }
 
   public BigInt times(BigInt other) {
@@ -36,7 +72,11 @@ public final class BigInt {
 
   @Override
   public String toString() {
-    throw new IllegalStateException("TODO task 4: implement creating representation");
+    String s = "";
+    if (!this.isPositive) s = "-";
+    for (int digit : this.digits)
+      s = s.concat(String.valueOf(digit));
+    return s;
   }
 
   @Override
