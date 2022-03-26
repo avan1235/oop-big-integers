95942b47eb6744c5ea99b8e0d1841acb94d529a1
m1kush <m1kush@disroot.org>
2022-03-26 16:17:37 +0100

    .

--- src/main/java/pl/edu/mimuw/BigInt.java
+++ src/main/java/pl/edu/mimuw/BigInt.java
@@ -1,60 +1,87 @@
 package pl.edu.mimuw;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 import static java.lang.Math.abs;
 import static java.lang.Math.min;
 import static java.util.Collections.swap;
 
-public final class BigInt {
+public final class BigInt
+{
 
   private final int[] digits;
 
   private final boolean isPositive;
 
-  public BigInt(String number) {
-    if (number.charAt(0) == '-') {
+  public BigInt(String number)
+  {
+    if (number.charAt(0) == '-')
+    {
       this.isPositive = false;
     }
     else this.isPositive = true;
     this.digits = new int[number.length() - (this.isPositive ? 0 : 1)];
-    for (int i = 0;i<this.digits.length;i++) {
-      this.digits[i] = number.charAt(i + (this.isPositive ? 0 : 1));
+    for (int i = 0;i<this.digits.length;i++)
+    {
+      this.digits[i] = number.charAt(i + (this.isPositive ? 0 : 1)) - '0';
     }
   }
 
-  public BigInt(int[] digits, boolean isPositive) {
+  public BigInt(int[] digits, boolean isPositive)
+  {
     this.digits = digits;
     this.isPositive = isPositive;
   }
 
-  public BigInt(int number) {
+  public BigInt(int number)
+  {
     this(Integer.toString(number));
   }
 
-  public BigInt neg() {
+  public BigInt neg()
+  {
     final var digits = Arrays.copyOf(this.digits, this.digits.length);
     return new BigInt(digits, !this.isPositive);
   }
 
-  public BigInt add(BigInt other) {
-    if (other.digits.length > this.digits.length)
-      return other.add(this);
+  public BigInt add(BigInt other)
+  {
     if (!this.isPositive)
       return this.neg().add(other.neg()).neg();
+    if (other.digits.length > this.digits.length)
+      return other.add(this);
     if (!other.isPositive)
       for (int i = 0;i<other.digits.length;i++)
         other.digits[i] = -other.digits[i];
 
-    for (int i = 0;i<other.digits.length-1;i++) {
+    for (int i = 0;i<other.digits.length-1;i++)
+    {
       this.digits[this.digits.length - i - 1] += other.digits[other.digits.length - i - 1];
       if (this.digits[this.digits.length - i - 1] > 9)
       {
         this.digits[this.digits.length - i - 1] -= 10;
         this.digits[this.digits.length - i - 2]++;
       }
+      if (this.digits[this.digits.length - i - 1] < 0)
+      {
+        this.digits[this.digits.length - i - 1] += 10;
+        this.digits[this.digits.length - i - 2]--;
+      }
     }
-    this.digits[0] += other.digits[0];
+    this.digits[this.digits.length - other.digits.length] += other.digits[0];
+    for (int i = this.digits.length - other.digits.length;i>0;i--) {
+      if (this.digits[i] > 9)
+      {
+        this.digits[i] -= 10;
+        this.digits[i - 1]++;
+      }
+      if (this.digits[i] < 0)
+      {
+        this.digits[i] += 10;
+        this.digits[i - 1]--;
+      }
+    }
     if (this.digits[0] > 9)
     {
       this.digits[0] -= 10;
@@ -63,24 +90,68 @@ public final class BigInt {
       System.arraycopy(this.digits, 0, ret, 1, this.digits.length);
       return new BigInt(ret, true);
     }
-
+    if (this.digits[0] < 0)
+    {
+      this.digits[0] = -this.digits[0];
+      for (int i = 1;i<this.digits.length;i++)
+        this.digits[i] = 10 - this.digits[i];
+      return this.neg();
+    }
+    return this;
   }
 
-  public BigInt times(BigInt other) {
-    throw new IllegalStateException("TODO task 3: implement multiplication");
+  public BigInt times(BigInt other)
+  {
+    if (!this.isPositive)
+      return this.neg().times(other.neg());
+    if (!other.isPositive)
+      return this.times(other.neg()).neg();
+    if (other.equals(new BigInt(10)))
+    {
+      if (this.equals(new BigInt(0)))
+        return new BigInt(0);
+      int[] tmp = new int[this.digits.length + 1];
+      System.arraycopy(this.digits, 0, tmp, 0, this.digits.length);
+      tmp[this.digits.length] = 0;
+      return new BigInt(tmp, true);
+    }
+    BigInt ret = new BigInt(0);
+    for (int i = 0;i<other.digits.length;i++)
+    {
+      ret = ret.times(new BigInt(10)).add(this.times(other.digits[i]));
+    }
+    return ret;
+  }
+
+  private BigInt times(int other)
+  {
+    BigInt ret = new BigInt(0);
+    for (int digit : this.digits)
+    {
+      ret = ret.times(new BigInt(10)).add(new BigInt(digit * other));
+    }
+    return ret;
   }
 
   @Override
-  public String toString() {
+  public String toString()
+  {
     String s = "";
     if (!this.isPositive) s = "-";
-    for (int digit : this.digits)
-      s = s.concat(String.valueOf(digit));
+    int i;
+    for (i = 0;i<this.digits.length;i++)
+      if (this.digits[i] != 0)
+        break;
+    if (i == this.digits.length)
+      return "0";
+    for (;i<this.digits.length;i++)
+      s = s.concat(String.valueOf(this.digits[i]));
     return s;
   }
 
   @Override
-  public int hashCode() {
+  public int hashCode()
+  {
     final var arrayHashCode = Arrays.hashCode(digits);
     final var positiveHashCode = Boolean.hashCode(isPositive);
 
@@ -88,7 +159,8 @@ public final class BigInt {
   }
 
   @Override
-  public boolean equals(Object obj) {
+  public boolean equals(Object obj)
+  {
     if (!(obj instanceof BigInt)) return false;
     final var other = (BigInt) obj;
 
--- src/test/java/pl/edu/mimuw/BigIntTest.java
+++ src/test/java/pl/edu/mimuw/BigIntTest.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class BigIntTest {
+class BigIntTest
+{
 
   @Test
-  void testSmallAdd() {
+  void testSmallAdd()
+  {
     final var x = new BigInt(42);
     final var y = new BigInt(-24);
     final var expectedResult = "18";
@@ -18,7 +20,8 @@ class BigIntTest {
   }
 
   @Test
-  void testBigAdd() {
+  void testBigAdd()
+  {
     final var x = new BigInt("-853487563485263495358327542");
     final var y = new BigInt("123432958723478523239");
     final var expectedResult = "-853487440052304771879804303";
@@ -29,17 +32,34 @@ class BigIntTest {
   }
 
   @Test
-  void testSmallTimes() {
-    throw new IllegalStateException("TODO task 5: write test for multiplication of small numbers");
+  void testSmallTimes()
+  {
+    for (int i = 0;i<20;i++)
+      for (int j = 0;j<20;j++)
+      {
+        var x = new BigInt(i);
+        var y = new BigInt(j);
+        var expectedResult = Integer.toString(i * j);
+        var result = x.times(y).toString();
+        assertEquals(expectedResult, result);
+      }
   }
 
   @Test
-  void testBigTimes() {
-    throw new IllegalStateException("TODO task 6: write test for multiplication of big numbers");
+  void testBigTimes()
+  {
+    final var x = new BigInt("-17258671298054151056129805");
+    final var y = new BigInt("-979124789426384953216754171");
+    final var expectedResult = "16898392900486464517411379656530173692104355351166655";
+
+    final var result = x.times(y).toString();
+
+    assertEquals(expectedResult, result);
   }
 
   @Test
-  void testNegation() {
+  void testNegation()
+  {
     final var small = new BigInt("37");
     final var big = new BigInt("-3453458734658736");
 
